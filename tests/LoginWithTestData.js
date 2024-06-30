e72a56e4503203162fb1a73a2c6624307b22149d
Nikhil <nikhilcogni@gmail.com>
2024-06-30 22:20:38 +0530

    Data Drive test Login

---
+++ tests/LoginWithTestData.js
@@ -0,0 +1,21 @@
+import {Selector, fixture} from 'testcafe'
+
+const dataSet = require("../Testdata/LoginData.json");
+
+fixture `Data Drive Test`
+   
+dataSet.forEach(data =>
+{
+    console.log(data);
+    test.page('https://the-internet.herokuapp.com/login')
+    (`Login Page - ${data.expectedResult}`, async t =>
+    {
+
+        await t.maximizeWindow();
+        await t.typeText("#username", data.username);
+        await t.typeText("#password", data.password);
+        await t.wait(3000);
+        await t.click("button i");
+        t.expect(Selector("div#flash").innerText, data.expectedResult);    
+    });
+})
